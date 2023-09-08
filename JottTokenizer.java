b5e664747ccc9b57e128a7fd923120f4982797d4
Issac Kim <idk4565@rit.edu>
2023-09-07 22:01:39 -0400

    Rewrote code to read filename as third second     command line input Began setup for DFA

--- JottTokenizer.java
+++ JottTokenizer.java
@@ -3,32 +3,79 @@
 /**
  * This class is responsible for tokenizing Jott code.
  * 
- * @author 
+ * @author Issac Kim
  **/
 
+import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class JottTokenizer {
 
+  /**
+   * java JottTokenizer.java Token.java [Insert file path to testcase]
+   * 
+   * @param args
+   */
+  public static void main(String[] args){
+    tokenize(args[1]);
+    System.out.println(args[1]);
+  }
+
 	/**
-     * Takes in a filename and tokenizes that file into Tokens
-     * based on the rules of the Jott Language
-     * @param filename the name of the file to tokenize; can be relative or absolute path
-     * @return an ArrayList of Jott Tokens
-     */
-    public static ArrayList<Token> tokenize(String filename){
+   * Takes in a filename and tokenizes that file into Tokens
+   * based on the rules of the Jott Language
+   * @param filename the name of the file to tokenize; can be relative or absolute path
+   * @return an ArrayList of Jott Tokens
+   */
+  public static ArrayList<Token> tokenize(String filename){
 
-    //turn file into a string
-    String fileString = Files.readString(filename);
+    //turns String filename into Path file
+    Path file = Paths.get(filename);
 
-    //goes through whole file string
-    for ( int i = 0; i < fileString.length(); i++ ) {
+    try{
 
+      //turns file into a string
+      String fileString = Files.readString(file);
+      int lineNum = 1;
+
+      //goes through whole file string
+      for ( int i = 0; i < fileString.length(); i++ ) {
+
+        char ch = fileString.charAt(i);
+
+        if(ch == '\n'){
+
+          lineNum++;
+          continue;
+
+        }
+
+        System.out.println(ch);
         //branches from DFA
-        
+          
+      }
+
+    } catch(IOException ioe){
+
+      //handles IOExce
+      System.out.println(ioe.toString());
+
+    } catch(OutOfMemoryError meme){
+
+      //handles MemError
+      System.out.println(meme.toString());
+
+    } catch(SecurityException sece){
+
+      //handles SecExe
+      System.out.println(sece.toString());
+
     }
 
 		return null;
 	}
+
 }
