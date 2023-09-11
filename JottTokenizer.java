8ded184b4cf4a7a133ee4504cf1867062ae75407
Issac Kim <idk4565@rit.edu>
2023-09-11 15:14:31 -0400

    Updated basic cases for ,[]{} to ensure no redundancy Added cases for 0-9

--- JottTokenizer.java
+++ JottTokenizer.java
@@ -42,43 +42,51 @@ public class JottTokenizer {
       //turns file into a string
       String fileString = Files.readString(file);
       String charStream = "";
-      TokenType type = TokenType.ASSIGN;
+      TokenType type = TokenType.ASSIGN; //placeholder to initalize variable
       int lineNum = 1;
+      int endOfFile = fileString.length();
 
       //goes through whole file string
-      for ( int i = 0; i < fileString.length(); i++ ) {
+      for ( int i = 0; i < endOfFile; i++ ) {
 
         char ch = fileString.charAt(i);
-
 
         switch(ch){
           case ',':
             Token token = (charStream == "") ? new Token(",", filename, lineNum, TokenType.COMMA) : new Token(charStream, filename, lineNum, type);
             tokenStream.add(token);
-            token = new Token(",", filename, lineNum, TokenType.COMMA);
-            tokenStream.add(token);
-
+            if(charStream != ""){
+              token = new Token(",", filename, lineNum, TokenType.COMMA);
+              tokenStream.add(token);
+            }
           case '[':
             token = (charStream == "") ? new Token("[", filename, lineNum, TokenType.L_BRACKET) : new Token(charStream, filename, lineNum, type);
             tokenStream.add(token);
-            token = new Token("[", filename, lineNum, TokenType.L_BRACKET);
-            tokenStream.add(token);
-
+            if(charStream != ""){
+              token = new Token("[", filename, lineNum, TokenType.L_BRACKET);
+              tokenStream.add(token);
+            }
           case ']':
             token = (charStream == "") ? new Token("]", filename, lineNum, TokenType.R_BRACKET) : new Token(charStream, filename, lineNum, type);
             tokenStream.add(token);
-            token = new Token("]", filename, lineNum, TokenType.R_BRACKET);
-            tokenStream.add(token);
+            if(charStream != ""){
+              token = new Token("]", filename, lineNum, TokenType.R_BRACKET);
+              tokenStream.add(token);
+            }
           case '{':
             token = (charStream == "") ? new Token("{", filename, lineNum, TokenType.L_BRACE) : new Token(charStream, filename, lineNum, type);
             tokenStream.add(token);
-            token = new Token("{", filename, lineNum, TokenType.L_BRACE);
-            tokenStream.add(token);          
+            if(charStream != ""){
+              token = new Token("{", filename, lineNum, TokenType.L_BRACE);
+              tokenStream.add(token);
+            }        
           case '}':
             token = (charStream == "") ? new Token("}", filename, lineNum, TokenType.R_BRACE) : new Token(charStream, filename, lineNum, type);
             tokenStream.add(token);
-            token = new Token("}", filename, lineNum, TokenType.R_BRACE);
-            tokenStream.add(token);
+            if(charStream != ""){
+              token = new Token("}", filename, lineNum, TokenType.R_BRACE);
+              tokenStream.add(token);
+            }
           case '=':
 
           case '<':
@@ -97,8 +105,28 @@ public class JottTokenizer {
 
           case '.':
 
+          case '0':
+
+          case '1':
+
+          case '2':
+
+          case '3':
+
+          case '4':
+
+          case '5': 
+
+          case '6':
+
+          case '7':
+
+          case '8':
+
+          case '9':
+
           case '#':
-            while(ch != '\n'){
+            while(ch != '\n' && i < endOfFile){
               i++;
               ch = fileString.charAt(i);
             }
