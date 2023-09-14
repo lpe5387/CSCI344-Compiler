f90b50628c1b4bad46a140cd83b0c45e087b9744
lpe5387 <90779890+lpe5387@users.noreply.github.com>
2023-09-14 18:18:38 -0400

    Began creating parse functions for some tree nodes

--- BoolNode.java
+++ BoolNode.java
@@ -3,12 +3,35 @@
  *
  * @author Luka Eaton
  */
+
+import java.util.ArrayList;
+
 public class BoolNode {
     
     private String value;
 
     public BoolNode(String value){
         this.value = value;
+    }
+
+    /**
+     * Function to parse a Boolean node for the parse tree.
+     * @param tokenlist the list of tokens made from the Tokenizer
+     * @return BoolNode
+     */
+    public static BoolNode ParseBool(ArrayList<Token> tokenlist){
+        Token token = tokenlist.get(0);
+        String tokenValue = token.getToken();
+        if(token.getTokenType() == TokenType.ID_KEYWORD && (tokenValue.equals("True") || 
+        tokenValue.equals("False"))){
+            BoolNode node = new BoolNode(token.getToken());
+            tokenlist.remove(0); 
+            return node;
+        }
+        else{
+            // throw new SyntaxException();
+            return null;
+        }
     }
 
     public String convertToJott(){return "";}
