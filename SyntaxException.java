9c960eb14c31673ab567732ed170e70bed93106a
lpe5387 <90779890+lpe5387@users.noreply.github.com>
2023-09-14 18:36:53 -0400

    small wording fix to the SyntaxException class

--- SyntaxException.java
+++ SyntaxException.java
@@ -1,5 +1,5 @@
 public class SyntaxException extends Exception{
     public SyntaxException(String errorMessage, String filename, int lineNum){
-        super("Syntax Error\n"+errorMessage+"\n"+filename+":"+lineNum+"\n");
+        super("Syntax Error:\n"+errorMessage+"\n"+filename+":"+lineNum+"\n");
     }
 }
