5389f29ee60af680812e53a937fa45fdae4e0eeb
lpe5387 <90779890+lpe5387@users.noreply.github.com>
2023-09-25 17:04:21 -0400

    implemented ParseBoolExpr

--- src/treeNodes/BoolExprNode.java
+++ src/treeNodes/BoolExprNode.java
@@ -4,12 +4,20 @@ This class is for the bool subset of the expression node
 
 Author: Luka Eaton
  */
+import java.util.ArrayList;
+import provided.Token;
+import exceptions.SyntaxException;
 public class BoolExprNode extends ExprNode{
 
     private BoolNode bool;
 
     public BoolExprNode(BoolNode bool){
         this.bool = bool;
+    }
+
+    public static BoolExprNode ParseBoolExpr(ArrayList<Token> tokenList) throws SyntaxException {
+        BoolNode bool = BoolNode.ParseBool(tokenList);
+        return new BoolExprNode(bool);
     }
 
 }
