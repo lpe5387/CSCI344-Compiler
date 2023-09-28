09006b5951a7630a858f86669f2987e57acffe41
lpe5387 <90779890+lpe5387@users.noreply.github.com>
2023-09-28 18:36:32 -0400

    deleting expression node stuff

--- src/treeNodes/ExprNode.java
+++ src/treeNodes/ExprNode.java
@@ -1,29 +1,18 @@
 package treeNodes;
 
 /**
- * This class is responsible for the expression node for the parse tree
+ * This interface is responsible for the expression node for the parse tree
  *
  * @author Luka Eaton
  */
 
-import provided.JottTree;
 import java.util.ArrayList;
 import provided.Token;
 import exceptions.SyntaxException;
-public abstract class ExprNode implements JottTree {
+public interface ExprNode {
 
     public static ExprNode ParseExpr(ArrayList<Token> tokenList){
         return null;
     }
-
-    public String convertToJott(){return "";}
-
-    public String convertToJava(String className){return "";}
-
-    public String convertToC(){return "";}
-
-    public String convertToPython(){return "";}
-    
-    public boolean validateTree(){return true;}
 
 }
