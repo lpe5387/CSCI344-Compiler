066ff5575891da03e099c1d67118362dc0760444
lpe5387 <90779890+lpe5387@users.noreply.github.com>
2023-09-28 20:35:44 -0400

    first implementation of my portion of phase 2

--- src/treeNodes/ExprNode.java
+++ src/treeNodes/ExprNode.java
@@ -9,10 +9,25 @@ package treeNodes;
 import java.util.ArrayList;
 import provided.Token;
 import exceptions.SyntaxException;
+import provided.TokenType;
+
 public interface ExprNode {
 
-    public static ExprNode ParseExpr(ArrayList<Token> tokenList){
-        return null;
+    public static ExprNode ParseExpr(ArrayList<Token> tokenlist) throws SyntaxException {
+        Token token = tokenlist.get(1);
+        ExprNode expr;
+        // if the second token is a math op / rel op, we are dealing with an operation
+        if(token.getTokenType() == TokenType.MATH_OP || token.getTokenType() == TokenType.REL_OP){
+            expr = OperationNode.parseOperation(tokenlist);
+        }
+        token = tokenlist.get(0);
+        if(token.getToken().equals("True") || token.getToken().equals("False")) expr = BoolNode.ParseBool(tokenlist);
+        else if(token.getTokenType() == TokenType.ID_KEYWORD) expr = IdNode.ParseId(tokenlist);
+        else if(token.getTokenType() == TokenType.NUMBER) expr = NumNode.ParseNum(tokenlist);
+        else if(token.getTokenType() == TokenType.STRING) expr = StringLiteralNode.ParseStringLiteral(tokenlist);
+        else if(token.getTokenType() == TokenType.FC_HEADER) expr = FuncCallNode.ParseFuncCall(tokenlist);
+        else throw new SyntaxException("Expected an Id, Number, Function call, Boolean, or String. Got: "+token.getToken(), token.getFilename(), token.getLineNum());
+        return expr;
     }
 
 }
--- src/treeNodes/OperationNode.java
+++ src/treeNodes/OperationNode.java
@@ -1,17 +1,37 @@
 package treeNodes;
+/**
+ * This class is intended to hold any operation with a left and right operand.
+ */
 
+import exceptions.SyntaxException;
 import provided.JottTree;
+import provided.Token;
+import provided.TokenType;
+
+import java.util.ArrayList;
 
 public class OperationNode implements JottTree, ExprNode {
 
-    private ExprNode expr1;
+    private ExprNode left;
     private OpNode op;
-    private ExprNode expr2;
+    private ExprNode right;
 
-    public OperationNode(ExprNode expr1, OpNode op, ExprNode expr2){
-        this.expr1 = expr1;
+    public OperationNode(ExprNode left, OpNode op, ExprNode right){
+        this.left = left;
         this.op = op;
-        this.expr2 = expr2;
+        this.right = right;
+    }
+
+    public static OperationNode parseOperation(ArrayList<Token> tokenlist) throws SyntaxException {
+        Token token = tokenlist.get(0);
+        ExprNode left;
+        if(token.getTokenType() == TokenType.ID_KEYWORD) left = IdNode.ParseId(tokenlist);
+        else if(token.getTokenType() == TokenType.NUMBER) left = NumNode.ParseNum(tokenlist);
+        else if(token.getTokenType() == TokenType.FC_HEADER) left = FuncCallNode.ParseFuncCall(tokenlist);
+        else throw new SyntaxException("Expected an Id, Number, or Function call. Got: "+token.getToken(), token.getFilename(), token.getLineNum());
+        OpNode op = OpNode.ParseOp(tokenlist);
+        ExprNode right = ExprNode.ParseExpr(tokenlist);
+        return new OperationNode(left, op, right);
     }
 
     public String convertToJott(){return "";}
