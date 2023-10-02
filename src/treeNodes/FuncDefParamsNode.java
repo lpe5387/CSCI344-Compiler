6ca1fac0ba732f4c5cc30d31f516388de4efc062
Issac Kim <idk4565@rit.edu>
2023-10-02 15:32:44 -0400

    implementation of funcdefparamsnode

--- src/treeNodes/FuncDefParamsNode.java
+++ src/treeNodes/FuncDefParamsNode.java
@@ -26,6 +26,22 @@ public class FuncDefParamsNode implements JottTree {
     }
 
     public static FuncDefParamsNode ParseFuncDefParams(ArrayList<Token> tokenlist){
+        Token token = tokenlist.get(0);
+        if(token.getTokenType() == TokenType.ID_KEYWORD){
+            IdNode idNode = IdNode.ParseId(tokenlist);
+            token = tokenlist.get(0);
+            if(token.getTokenType() == TokenType.COLON) {
+                TypeNode type = TypeNode.parseType(tokenlist);
+                ArrayList<FuncDefParamsTNode> funcDefParamsTNodes = new ArrayList<>();
+                while (tokenlist.get(0).getTokenType() == TokenType.COMMA) {
+                    funcDefParamsTNodes.add(FuncDefParamsTNode.ParseFuncDefParamsT(tokenlist));
+                }
+                FuncDefParamsNode node = new FuncDefParamsNode(idNode, type, funcDefParamsTNodes);
+                return node;
+            }
+        } else {
+            return null;
+        }
 
     }
 
