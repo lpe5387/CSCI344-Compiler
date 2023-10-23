a1a06bba8a0acd481abaffdcc261799742370427
ad8182 <ad8182@rit.edu>
2023-10-23 16:54:11 -0400

    Expanded JottMain to have 2 params (Filename and Lnaguage) and validate itss 2 params before running the 3 phases of the program

--- src/JottMain.java
+++ src/JottMain.java
@@ -1,4 +1,8 @@
 import provided.*;
+import treeNodes.ProgramNode;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 /**
@@ -10,10 +14,48 @@ import java.util.ArrayList;
 public class JottMain {
     public static void main (String[] args) {
         try {
-            ArrayList <Token> tokenList = JottTokenizer.tokenize(args[1]);
-            System.out.println(tokenList.toString());
+            // STEP 0: validate args.
+            if(args.length != 2){ //must have 2 args
+                System.err.println("Usage: JottMain FILENAME Language.\n" +
+                        "Where:\n" +
+                        "FILENAME is the name of the Jott file to be parsed,\n" +
+                        "and Language is \"Jott\", \"Java\", \"C\", or \"Python\" depending on which language you want the result in.");
+            }
+            //must have the 2nd arg be a language we translate to
+            else if(!args[1].equals("Jott") && !args[1].equals("Java") && !args[1].equals( "C") && !args[1].equals("Python")){
+                System.err.println("Usage: JottMain FILENAME Language.\n" +
+                        "Where:\n" +
+                        "FILENAME is the name of the Jott file to be parsed,\n" +
+                        "and Language is \"Jott\", \"Java\", \"C\", or \"Python\" depending on which language you want the result in.");
+            }
+            else {
+                boolean valid = false;
+                try{
+                    Path file = Paths.get(args[0]);
+                    file = file.toAbsolutePath(); //redundancy in file pathing where CSCI344 repeated
+                    valid = true; //if we get here without breaking we are good to run the program
+                }
+                catch (Exception E){
+                    System.err.println("Failed to open " + args[0] + " See error below.");
+                    System.err.println(E.toString());
+                    System.err.println("Usage: JottMain FILENAME Language.\n" +
+                            "Where:\n" +
+                            "FILENAME is the name of the Jott file to be parsed,\n" +
+                            "and Language is \"Jott\", \"Java\", \"C\", or \"Python\" depending on which language you want the result in.");
+                }
+                if(valid) {
+                    // STEP 1: Tokenize
+                    ArrayList<Token> tokenList = JottTokenizer.tokenize(args[0]);
+                    // STEP 2: Parse into jott tree
+                    ProgramNode program = ProgramNode.parseProgram(tokenList);
+                    // STEP 3: Make the Parse table and validate tree
+                    //TODO: make the parse table
+                    program.validateTree();
+                    // STEP 4: Do Phase 4 things.
+                }
+            }
         } catch (Exception exception) {
-            System.out.println(exception.toString());
+            System.err.println(exception.toString());
         }
     }    
 }
