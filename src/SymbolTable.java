938d889a8fb200dcb17e07bfa3a04c653982f21b
dp8523 <dp8523@g.rit.edu>
2023-10-26 18:06:30 -0400

    created and implemented SymbolTable class

---
+++ src/SymbolTable.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * This class is responsible for storing all the defined functions, and variables within their scopes.
+ *
+ * @author Dara Prak, Luka Eaton, Lucie Lim, Andrew Dantone
+ */
+
+public class SymbolTable {
+    private static HashMap<String, ArrayList<String>> funcDefs = new HashMap<>();
+    private static HashMap<String, HashMap<String, ArrayList<String>>> varDefs = new HashMap<>();
+    private static String currentScope = null;
+
+    public static ArrayList<String> getFuncDef(String funcName) {
+        return funcDefs.get(funcName);
+    }
+
+    public static ArrayList<String> getVarDef(String scope, String varName) {
+        return varDefs.get(scope).get(varName);
+    }
+
+    public static void addFuncDef(String funcName, ArrayList<String> funcDetails) {
+        funcDefs.put(funcName, funcDetails);
+        varDefs.put(funcName, new HashMap<String, ArrayList<String>>());
+    }
+
+    public static void addVarDef(String varName, ArrayList<String> varDetails) {
+        HashMap<String, ArrayList<String>> varMap = varDefs.get(currentScope);
+        varMap.put(varName, varDetails);
+    }
+
+    public static void setCurrentScope(String scope) {
+        currentScope = scope;
+    }
+}
