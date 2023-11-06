3d2149de665c1a64d4e31c2afe7d18d76aa77f63
ad8182 <ad8182@rit.edu>
2023-11-06 16:00:58 -0500

    Changed JottMain to acocunt for writing to an output file

--- src/JottMain.java
+++ src/JottMain.java
@@ -4,6 +4,8 @@ import provided.*;
 import treeNodes.ProgramNode;
 import java.io.File;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -18,34 +20,40 @@ public class JottMain {
     public static void main (String[] args) {
         try {
             // STEP 0: validate args.
-            if(args.length != 2){ //must have 2 args
-                System.err.println("Usage: JottMain FILENAME Language.\n" +
+            if(args.length != 3){ //must have 3 args
+                System.err.println("Usage: JottMain INPUTFILENAME OUTPUTFILENAME Language.\n" +
                         "Where:\n" +
-                        "FILENAME is the name of the Jott file to be parsed,\n" +
+                        "INPUTFILENAME is the name of the Jott file to be parsed,\n" +
+                        "OUTPUTFILENAME is the name of the output file to be written into\n" +
                         "and Language is \"Jott\", \"Java\", \"C\", or \"Python\" depending on which language you want the result in.");
             }
-            //must have the 2nd arg be a language we translate to
-            else if(!args[1].equals("Jott") && !args[1].equals("Java") && !args[1].equals("C") && !args[1].equals("Python")){
-                System.err.println("Usage: JottMain FILENAME Language.\n" +
+            //must have the 3nd arg be a language we translate to
+            else if(!args[2].equals("Jott") && !args[2].equals("Java") && !args[2].equals("C") && !args[2].equals("Python")){
+                System.err.println("Usage: JottMain INPUTFILENAME OUTPUTFILENAME Language.\n" +
                         "Where:\n" +
-                        "FILENAME is the name of the Jott file to be parsed,\n" +
+                        "INPUTFILENAME is the name of the Jott file to be parsed,\n" +
+                        "OUTPUTFILENAME is the name of the output file to be written into\n" +
                         "and Language is \"Jott\", \"Java\", \"C\", or \"Python\" depending on which language you want the result in.");
             }
             else {
-                boolean exists = false;
+                boolean existsInput = false;
+                boolean existsOutput = false;
                 try{
-                    File file = new File(args[0]);
-                    exists = file.isFile();
+                    File fileInput = new File(args[0]);
+                    File fileOutput = new File(args[1]);
+                    existsInput = fileInput.isFile();
+                    existsOutput = fileOutput.isFile();
                 }
                 catch (Exception E){
-                    System.err.println("Failed to open " + args[0] + ", See error below.");
+                    System.err.println("Failed to open " + args[0] + "or " + args[1] + ", See error below.");
                     System.err.println(E.toString());
-                    System.err.println("Usage: JottMain FILENAME Language.\n" +
+                    System.err.println("Usage: JottMain INPUTFILENAME OUTPUTFILENAME Language.\n" +
                             "Where:\n" +
-                            "FILENAME is the name of the Jott file to be parsed,\n" +
+                            "INPUTFILENAME is the name of the Jott file to be parsed,\n" +
+                            "OUTPUTFILENAME is the name of the output file to be written into\n" +
                             "and Language is \"Jott\", \"Java\", \"C\", or \"Python\" depending on which language you want the result in.");
                 }
-                if(exists) {
+                if(existsInput && existsOutput) {
                     // STEP 1: Tokenize
                     ArrayList<Token> tokenList = JottTokenizer.tokenize(args[0]);
                     // STEP 2: Parse into jott tree
@@ -53,12 +61,34 @@ public class JottMain {
                     // STEP 3: Make the Parse table and validate tree
                     program.validateTree();
                     // STEP 4: Do Phase 4 things.
+                    try {
+                        FileWriter writer = new FileWriter(args[1]);
+                        if (args[2].equals("Jott")) {
+                            writer.write(program.convertToJott());
+                        } else if (args[2].equals("Java")) {
+                            writer.write(program.convertToJava(args[0])); //this might be wrong idk, that's not this phase
+                        } else if (args[2].equals("C")) {
+                            writer.write(program.convertToC());
+                        } else if (args[2].equals("Python")) {
+                            writer.write(program.convertToPython());
+                        }
+                        writer.close();
+                    }
+                    catch (IOException e){
+                        System.err.println("There was a problem writing to " + args[1]);
+                        System.err.println("Usage: JottMain INPUTFILENAME OUTPUTFILENAME Language.\n" +
+                                "Where:\n" +
+                                "INPUTFILENAME is the name of the Jott file to be parsed,\n" +
+                                "OUTPUTFILENAME is the name of the output file to be written into\n" +
+                                "and Language is \"Jott\", \"Java\", \"C\", or \"Python\" depending on which language you want the result in.");
+                    }
                 }
                 else{
                     System.err.println("File " + args[0] + " does not exist");
-                    System.err.println("Usage: JottMain FILENAME Language.\n" +
+                    System.err.println("Usage: JottMain INPUTFILENAME OUTPUTFILENAME Language.\n" +
                             "Where:\n" +
-                            "FILENAME is the name of the Jott file to be parsed,\n" +
+                            "INPUTFILENAME is the name of the Jott file to be parsed,\n" +
+                            "OUTPUTFILENAME is the name of the output file to be written into\n" +
                             "and Language is \"Jott\", \"Java\", \"C\", or \"Python\" depending on which language you want the result in.");
                 }
             }
