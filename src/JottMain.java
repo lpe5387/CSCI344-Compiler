ea6256229b31e01ca15ec1234dcbf031459b19ec
ad8182 <ad8182@rit.edu>
2023-11-07 18:03:57 -0500

    Updated JottMain

--- src/JottMain.java
+++ src/JottMain.java
@@ -35,7 +35,16 @@ public class JottMain {
                         "OUTPUTFILENAME is the name of the output file to be written into\n" +
                         "and Language is \"Jott\", \"Java\", \"C\", or \"Python\" depending on which language you want the result in.");
             }
+            else if(args[0].equals(args[1])){
+                System.err.println("INPUTFILENAME and OUTPUTFILENAME cannot be the same\n" +
+                        "Usage: JottMain INPUTFILENAME OUTPUTFILENAME Language.\n" +
+                        "Where:\n" +
+                        "INPUTFILENAME is the name of the Jott file to be parsed,\n" +
+                        "OUTPUTFILENAME is the name of the output file to be written into\n" +
+                        "and Language is \"Jott\", \"Java\", \"C\", or \"Python\" depending on which language you want the result in.");
+            }
             else {
+                Boolean exception = false;
                 boolean existsInput = false;
                 boolean existsOutput = false;
                 try{
@@ -45,6 +54,7 @@ public class JottMain {
                     existsOutput = fileOutput.isFile();
                 }
                 catch (Exception E){
+                    exception = true;
                     System.err.println("Failed to open " + args[0] + "or " + args[1] + ", See error below.");
                     System.err.println(E.toString());
                     System.err.println("Usage: JottMain INPUTFILENAME OUTPUTFILENAME Language.\n" +
@@ -53,7 +63,7 @@ public class JottMain {
                             "OUTPUTFILENAME is the name of the output file to be written into\n" +
                             "and Language is \"Jott\", \"Java\", \"C\", or \"Python\" depending on which language you want the result in.");
                 }
-                if(existsInput && existsOutput) {
+                if(existsInput && existsOutput && !exception) {
                     // STEP 1: Tokenize
                     ArrayList<Token> tokenList = JottTokenizer.tokenize(args[0]);
                     // STEP 2: Parse into jott tree
@@ -75,7 +85,38 @@ public class JottMain {
                         writer.close();
                     }
                     catch (IOException e){
-                        System.err.println("There was a problem writing to " + args[1]);
+                        System.err.println("There was a problem with writing to the file: " + args[1]);
+                        System.err.println("Usage: JottMain INPUTFILENAME OUTPUTFILENAME Language.\n" +
+                                "Where:\n" +
+                                "INPUTFILENAME is the name of the Jott file to be parsed,\n" +
+                                "OUTPUTFILENAME is the name of the output file to be written into\n" +
+                                "and Language is \"Jott\", \"Java\", \"C\", or \"Python\" depending on which language you want the result in.");
+                    }
+                }
+                else if(existsInput && !existsOutput && !exception) {
+                    // STEP 1: Tokenize
+                    ArrayList<Token> tokenList = JottTokenizer.tokenize(args[0]);
+                    // STEP 2: Parse into jott tree
+                    ProgramNode program = ProgramNode.parseProgram(tokenList);
+                    // STEP 3: Make the Parse table and validate tree
+                    program.validateTree();
+                    // STEP 4: Do Phase 4 things.
+                    // TODO: create the file in the new directory
+                    try {
+                        FileWriter writer = new FileWriter(args[1]);
+                        if (args[2].equals("Jott")) {
+                            writer.write(program.convertToJott());
+                        } else if (args[2].equals("Java")) {
+                            writer.write(program.convertToJava(args[0])); //this might be wrong idk, that's not this phase
+                        } else if (args[2].equals("C")) {
+                            writer.write(program.convertToC());
+                        } else if (args[2].equals("Python")) {
+                            writer.write(program.convertToPython());
+                        }
+                        writer.close();
+                    }
+                    catch (IOException e){
+                        System.err.println("There was a problem with writing to the file: " + args[1]);
                         System.err.println("Usage: JottMain INPUTFILENAME OUTPUTFILENAME Language.\n" +
                                 "Where:\n" +
                                 "INPUTFILENAME is the name of the Jott file to be parsed,\n" +
@@ -84,7 +125,7 @@ public class JottMain {
                     }
                 }
                 else{
-                    System.err.println("File " + args[0] + " does not exist");
+                    System.err.println("File " + args[0] + " does not exist." + "Or file " + args[1] + " does not exist");
                     System.err.println("Usage: JottMain INPUTFILENAME OUTPUTFILENAME Language.\n" +
                             "Where:\n" +
                             "INPUTFILENAME is the name of the Jott file to be parsed,\n" +
