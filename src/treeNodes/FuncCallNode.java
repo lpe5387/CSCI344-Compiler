08c9e9e6831228c878685840e58d8ee14cad78a0
lucie lim <90916921+ll5880@users.noreply.github.com>
2023-11-09 17:56:28 -0500

    fixed grammer fo print statements

--- src/treeNodes/FuncCallNode.java
+++ src/treeNodes/FuncCallNode.java
@@ -97,18 +97,18 @@ public class FuncCallNode implements ExprNode, BodyStmtNode {
     public boolean validateTree() throws SemanticException{
         //Make sure function name (id) is in the symbol table
         if(SymbolTable.getFuncDef(this.id.getToken().getToken()) == null){
-            throw new SemanticException("Called the function '" + this.id.getToken().getToken() + "' without declaring it.", this.id.getToken().getFilename(), this.id.getToken().getLineNum());
+            throw new SemanticException("Called the function '" + this.id.getToken().getToken() + "' without defining it.", this.id.getToken().getFilename(), this.id.getToken().getLineNum());
         }
         //if we are here then the function exists, so now we need the params
         ArrayList<String> funcDef = SymbolTable.getFuncDef(this.id.getToken().getToken());
         int numParams = funcDef.size()-1; //funcDef holds all the params and the return type
         if(this.params.getIsEmpty() && numParams != 0){
             //if we are here then we are supposed to have at least one param and have been given 0
-            throw new SemanticException("Received no parameters for function " + this.id.getToken().getToken() + " which requires at least one parameter when previously declared.", this.id.getToken().getFilename(), this.id.getToken().getLineNum());
+            throw new SemanticException("Received no parameters for function " + this.id.getToken().getToken() + " which requires at least one parameter when previously defined.", this.id.getToken().getFilename(), this.id.getToken().getLineNum());
         }
         else if(!this.params.getIsEmpty() && numParams == 0){
             //if we are here then we are supposed to have 0 params, but we are given at least one
-            throw new SemanticException("Received at least one parameter for function " + this.id.getToken().getToken() + " which requires no parameters when previously declared.", this.id.getToken().getFilename(), this.id.getToken().getLineNum());
+            throw new SemanticException("Received at least one parameter for function " + this.id.getToken().getToken() + " which requires no parameters when previously defined.", this.id.getToken().getFilename(), this.id.getToken().getLineNum());
         }
         else if(this.params.getIsEmpty() && numParams == 0){
             //if we are here then we got 0 params, and we are supposed to have 0 params, since we already validated the name, we are good
